fe289e0662ed88506ab0b337a5d226bd31a542ec
Juliusz Ryłko <juliusz.rylko@gmail.com>
2019-11-01 19:32:17 +0100

    more tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,2 +1,22 @@
 def test_siema():
     assert True
+
+
+def test_siema1():
+    assert False
+
+
+def test_siema2():
+    assert False
+
+
+def test_siema3():
+    assert True
+
+
+def test_siema4():
+    assert True
+
+
+def test_siema5():
+    assert True
